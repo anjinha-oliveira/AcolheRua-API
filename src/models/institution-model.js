cb1b11b27ea8cd8dbfdc506b2a6d487cacd6a4a4
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-07-13 14:18:25 -0300

    Implementar tabela e modelo e instituição #4

---
+++ src/models/institution-model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const InstitutionSchema = new mongoose.Schema(
+    {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId()
+        },
+
+        name: {
+            type: String, 
+            required: true
+        },
+
+        address: {
+            type: String,
+            required: true
+        },
+
+        phone: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type: String,
+            required: true
+        }
+    }
+)
+
+const Model = mongoose.model('Institution', InstitutionSchema)
+
+module.exports = Model
